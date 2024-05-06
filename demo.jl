e574ab9501f00843654000965a084f4423821433
Steffen Plunder <steffen.plunder@web.de>
2024-05-07 02:14:01 +0900

    cleanup

--- demo.jl
+++ demo.jl
@@ -26,202 +26,203 @@ using StaticArrays
 
 const SVec3 = SVector{3, Float32}
 
-targetFps = 60
-numSubsteps = 30
-timeStep = 1.0 / 60.0
-gravity = SVec3(0, 0, -10)
-paused = false 
-hidden = false 
-frameNr = 0
+function init_state(;
+        numSubsteps = 30,
+        timeStep = 1.0f0 / 60.0f0,
+        gravity = SVec3(0, 0, -10),
+        sphereCenter = SVec3(0, 0, 1.5),
+        sphereRadius = 0.5f0,
+        numX = 500,
+        numY = 500,
+        spacing = 0.01f0,
+        zOffset = 2.2f0
+    )
+    # targetFps = 60
+    # paused = false 
+    # hidden = false 
+    # frameNr = 0
 
-clothNumX = 500
-clothNumY = 500
-clothY = 2.2
-clothSpacing = 0.01
-sphereCenter = SVec3(0, 0, 1.5)
-sphereRadius = 0.5
+    # dragParticleNr = -1
+    # dragDepth = 0.0
+    # dragInvMass = 0.0
 
+    numX += numX % 2
+    numY += numY % 2 
 
-#function init(zOffset, numX, numY, spacing, sphereCenter, sphereRadiuss)
-
-numX = clothNumX
-numY = clothNumY
-spacing = clothSpacing
-zOffset = clothY
-
-dragParticleNr = -1
-dragDepth = 0.0
-dragInvMass = 0.0
-
-numX += numX % 2
-numY += numY % 2 
-
-numParticles = (numX + 1) * (numY + 1)
-hostPos = zeros(SVec3, numX + 1, numY + 1)
-hostNormals = zeros(SVec3, numX + 1, numY + 1)
-hostInvMass = ones(numParticles)
-
-hostTriIds = Vector{GLTriangleFace}(undef, 2 * numX * numY)
-k = 1
-
-for I in CartesianIndices(hostPos)
-    id = LinearIndices(hostPos)[I]
-
-    hostPos[I] = SVec3(
-                (-numX * 0.5 + I[1] - 1)*spacing, 
-                (-numY * 0.5 + I[2] - 1)*spacing,
-                zOffset
-            )
-
-    if I[1] <= numX && I[2] <= numY
-        tI = Tuple(I)
-        hostTriIds[k] = GLTriangleFace(id, id + 1, id + numY + 1)
-        hostTriIds[k+1] = GLTriangleFace(id + 1, id + numY + 2, id + numY + 1)
-        k += 2
-    end
-end
-
-pos = cu(hostPos)
-prevPos = cu(hostPos)
-restPos = cu(hostPos)
-invMass = cu(hostInvMass)
-corr = CUDA.zeros(SVec3, numX+1, numY+1)
-vel = CUDA.zeros(SVec3, numX+1, numY+1)
-normals = cu(hostNormals)
-triIds = cu(hostTriIds)
+    numParticles = (numX + 1) * (numY + 1)
+    hostPos = zeros(SVec3, numX + 1, numY + 1)
+    # hostNormals = zeros(SVec3, 2 * numX * numY)
+    hostInvMass = ones(Float32, numParticles)
+    hostTriIds = Vector{GLTriangleFace}(undef, 2 * numX * numY)
 
 
-backend = CUDABackend()
-
-# constraints
-
-passSizes = (
-    (numX + 1) * floor(Int, numY / 2),
-    (numX + 1) * floor(Int, numY / 2),
-    floor(Int, numX / 2) * (numY + 1),
-    floor(Int, numX / 2) * (numY + 1),
-    2 * numX * numY + (numX + 1) * (numY - 1) + (numY + 1) * (numX - 1)
-)
-
-
-passIndependent = (true, true, true, true, false)
-
-numDistConstraints = sum(passSizes)
-
-hostDistConstIds = Array{NTuple{2, Int32}}(undef, numDistConstraints)
-
-# stretch constraints
-begin
     k = 1
-    for passNr in 1:2
-        for i in passNr:2:size(pos,1)-1, j in 1:size(pos, 2)
-            hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i+1,j])
+    for I in CartesianIndices(hostPos)
+        id = LinearIndices(hostPos)[I]
+
+        hostPos[I] = SVec3(
+                    (-numX * 0.5 + I[1] - 1)*spacing, 
+                    (-numY * 0.5 + I[2] - 1)*spacing,
+                    zOffset
+                )
+
+        if I[1] <= numX && I[2] <= numY
+            tI = Tuple(I)
+            hostTriIds[k] = GLTriangleFace(id, id + 1, id + numY + 1)
+            hostTriIds[k+1] = GLTriangleFace(id + 1, id + numY + 2, id + numY + 1)
+            k += 2
+        end
+    end
+
+    pos = cu(hostPos)
+    prevPos = cu(hostPos)
+    restPos = cu(hostPos)
+    invMass = cu(hostInvMass)
+    corr = CUDA.zeros(SVec3, numX+1, numY+1)
+    vel = CUDA.zeros(SVec3, numX+1, numY+1)
+    # normals = cu(hostNormals)
+    triIds = cu(hostTriIds)
+
+    # constraints
+    passSizes = (
+        (numX + 1) * floor(Int, numY / 2),
+        (numX + 1) * floor(Int, numY / 2),
+        floor(Int, numX / 2) * (numY + 1),
+        floor(Int, numX / 2) * (numY + 1),
+        2 * numX * numY + (numX + 1) * (numY - 1) + (numY + 1) * (numX - 1)
+    )
+
+
+    passIndependent = (true, true, true, true, false)
+
+    numDistConstraints = sum(passSizes)
+
+    hostDistConstIds = Array{NTuple{2, Int32}}(undef, numDistConstraints)
+
+    # stretch constraints
+    begin
+        k = 1
+        for passNr in 1:2
+            for i in passNr:2:size(pos,1)-1, j in 1:size(pos, 2)
+                hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i+1,j])
+                k += 1
+            end
+        end
+
+        for passNr in 1:2
+            for i in 1:size(pos, 1), j in passNr:2:size(pos,2)-1
+                hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i,j+1])
+                k += 1
+            end
+        end
+
+        # shear constraints 
+
+        for i in 1:size(pos,1)-1, j in 1:size(pos,2)-1
+            hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i+1,j+1])
+            k += 1
+            hostDistConstIds[k] = (LinearIndices(pos)[i+1,j], LinearIndices(pos)[i,j+1])
+            k += 1
+        end
+
+        # bending constraints 
+
+        for i in 1:size(pos,1)-2, j in 1:size(pos,2)
+            hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i+2,j])
+            k += 1
+        end
+
+        for i in 1:size(pos,1), j in 1:size(pos,2)-2
+            hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i,j+2])
             k += 1
         end
     end
 
-    for passNr in 1:2
-        for i in 1:size(pos, 1), j in passNr:2:size(pos,2)-1
-            hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i,j+1])
-            k += 1
-        end
+    distConstIds = cu(hostDistConstIds)
+
+    @kernel function computeRestLengths(pos, constIds, restLengths)
+        cNr = @index(Global)
+        p0 = pos[constIds[cNr][1]]
+        p1 = pos[constIds[cNr][2]]
+        restLengths[cNr] = norm(p0 - p1)
     end
 
-    # shear constraints 
+    constRestLengths = CUDA.zeros(numDistConstraints)
 
-    for i in 1:size(pos,1)-1, j in 1:size(pos,2)-1
-        hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i+1,j+1])
-        k += 1
-        hostDistConstIds[k] = (LinearIndices(pos)[i+1,j], LinearIndices(pos)[i,j+1])
-        k += 1
-    end
+    backend = get_backend(pos)
+    computeRestLengths(backend, 64)(pos, distConstIds, constRestLengths, ndrange = numDistConstraints)
+    KernelAbstractions.synchronize(backend)
 
-    # bending constraints 
 
-    for i in 1:size(pos,1)-2, j in 1:size(pos,2)
-        hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i+2,j])
-        k += 1
-    end
+    state = (;
+        pos, 
+        vel,
+        triIds,
+        # normals,
+        prevPos,
+        invMass,
+        corr,
+        passSizes, 
+        passIndependent,
+        distConstIds,
+        constRestLengths, 
+        restPos,
+        hostPos,
+        hostTriIds,
+        # hostNormals
+    )
 
-    for i in 1:size(pos,1), j in 1:size(pos,2)-2
-        hostDistConstIds[k] = (LinearIndices(pos)[i,j], LinearIndices(pos)[i,j+2])
-        k += 1
-    end
+    params = (;
+        timeStep,    
+        numSubsteps,
+        gravity, 
+        sphereCenter = sphereCenter + SVec3(0,0,0),
+        sphereRadius,
+        jacobiScale = 0.2f0
+    )
+
+    return (;state, params)
 end
-
-distConstIds = cu(hostDistConstIds)
-
-@kernel function computeRestLengths(pos, constIds, restLengths)
-    cNr = @index(Global)
-    p0 = pos[constIds[cNr][1]]
-    p1 = pos[constIds[cNr][2]]
-    restLengths[cNr] = norm(p0 - p1)
-end
-
-constRestLengths = CUDA.zeros(numDistConstraints)
-
-computeRestLengths(backend, 64)(pos, distConstIds, constRestLengths, ndrange = numDistConstraints)
-KernelAbstractions.synchronize(backend)
-
 
 # helper function to support atomic add of static vectors
 function vecAtomicAdd(x, i, y::SVec3)
-    x_ = reshape(reinterpret(Float32, x), 3, size(x,1) * size(x,2))
-
+    x_ = reshape(reinterpret(Float32, x), 3, length(x))
     Atomix.@atomic x_[1,i] += y[1]
     Atomix.@atomic x_[2,i] += y[2]
     Atomix.@atomic x_[3,i] += y[3]
 end
 
-function vecAtomicAdd(x, i, y::SVector)
-    x_ = reshape(reinterpret(Float32, x), length(y), length(x))
-
-    map(eachindex(y)) do j 
-        Atomix.@atomic x_[j,i] += y[j]
-    end
-end
-
 # compute normals 
+# @kernel function addNormals(@Const(pos), @Const(triIds), normals)
+#     triNr = @index(Global)
 
-@kernel function addNormals(@Const(pos), @Const(triIds), normals)
-    triNr = @index(Global)
+#     id0, id1, id2 = convert.(UInt32, triIds[triNr])
+#     normal = cross(pos[id1] - pos[id0], pos[id2] - pos[id0])
+#     vecAtomicAdd(normals, id0, normal)
+#     vecAtomicAdd(normals, id1, normal)
+#     vecAtomicAdd(normals, id2, normal)
+# end
 
-    id0, id1, id2 = convert.(UInt32, triIds[triNr])
-    normal = cross(pos[id1] - pos[id0], pos[id2] - pos[id0])
-    vecAtomicAdd(normals, id0, normal)
-    vecAtomicAdd(normals, id1, normal)
-    vecAtomicAdd(normals, id2, normal)
-end
+# @kernel function normalizeNormals(normals)
+#     normalId = @index(Global)
+#     normals[normalId] = normalize(normals[normalId]) 
+# end
 
-@kernel function normalizeNormals(normals)
-    normalId = @index(Global)
-    normals[normalId] = normalize(normals[normalId]) 
-end
-
-function updateMesh(normals, pos, triIds, backend = CUDABackend())
-    fill!(normals, zero(SVec3))
-    addNormals(backend, 64)(pos, triIds, normals, ndrange = length(triIds))
-    normalizeNormals(backend, 64)(normals, ndrange = length(normals))
-    KernelAbstractions.synchronize(backend)
-    copy!(hostNormals, normals)
-end
-
-updateMesh(normals, pos, triIds)
 
 @kernel function integrate(dt, gravity, invMass, prevPos, pos, vel, sphereCenter, sphereRadius)
     pNr = @index(Global)
 
     prevPos[pNr] = pos[pNr]
-    if invMass[pNr] != 0.0
+    if invMass[pNr] != 0.0f0
         vel[pNr] += gravity * dt 
         pos[pNr] += vel[pNr] * dt 
 
-        thickness = 0.001 
-        friction = 0.01
+        thickness = 0.001f0
+        friction = 0.01f0
 
         d = norm(pos[pNr] - sphereCenter)
         if d < (sphereRadius + thickness)
-            p = pos[pNr] * (1.0 - friction) + prevPos[pNr] * friction 
+            p = pos[pNr] * (1.0f0 - friction) + prevPos[pNr] * friction 
             r = p - sphereCenter
             d = norm(r)
             pos[pNr] = sphereCenter + r * ((sphereRadius + thickness) / d)
@@ -229,7 +230,7 @@ updateMesh(normals, pos, triIds)
 
         p = pos[pNr]
         if p[3] < thickness
-            p = pos[pNr] * (1 - friction) + prevPos[pNr] * friction 
+            p = pos[pNr] * (1.0f0 - friction) + prevPos[pNr] * friction 
             pos[pNr] = SVec3(p[1], p[2], thickness)
         end
     end
@@ -273,6 +274,7 @@ end
 
 function simulate!(s, p, backend = CUDABackend(), nworkgroups = 256)  # state, params
     (;pos, vel, prevPos, hostPos, invMass, corr, distConstIds, constRestLengths, passSizes, passIndependent) = s
+    (;normals, triIds, hostNormals) = s
     (;timeStep, numSubsteps, gravity, sphereCenter, sphereRadius, jacobiScale) = p
     
     dt = timeStep / numSubsteps
@@ -299,8 +301,15 @@ function simulate!(s, p, backend = CUDABackend(), nworkgroups = 256)  # state, p
 
         #TODO: add constraint solvers
     end
-    KernelAbstractions.synchronize(backend)
+    
+    # update normals 
+    # fill!(normals, zero(SVec3))
+    # addNormals(backend, nworkgroups)(pos, triIds, normals, ndrange = length(triIds))
+    # normalizeNormals(backend, nworkgroups)(normals, ndrange = length(normals))
+    # KernelAbstractions.synchronize(backend)
+
     copy!(hostPos, pos)
+    # copy!(hostNormals, normals)
 end
 
 
@@ -313,88 +322,69 @@ end
 
 
 
+# visualisation
 
+function init_plot(state, params)
+    
+    # prepare data pipeline
+    stateObs = Observable(state)
+    msh = @lift GeometryBasics.Mesh(reinterpret(Point3f, vec($stateObs.hostPos)), state.hostTriIds)
+    
+    # lights
+    lights = [
+        DirectionalLight(RGBf(0, 0, 0.7), Vec3f(-4, -4, 0)),
+        DirectionalLight(RGBf(0.7, 0.2, 0), Vec3f(-4, 4, -1)),
+        DirectionalLight(RGBf(0.7, 0.7, 0.7), Vec3f(4, -4, -1)),
+        AmbientLight(RGBf(0.3, 0.2, 0.2))
+    ]
 
-
-state = (;
-    pos, 
-    prevPos,
-    restPos,
-    invMass,
-    corr,
-    vel,
-    normals,
-    timeStep,
-    hostPos,
-    hostTriIds,
-    distConstIds, constRestLengths, passSizes, passIndependent
-)
-
-params = (;
-    timeStep,    
-    numSubsteps,
-    gravity, 
-    sphereCenter = sphereCenter + SVec3(0,0,0),
-    sphereRadius,
-    jacobiScale = 0.2
-)
-
-
-
-
-posObs = Observable(hostPos)
-msh = @lift normal_mesh(reinterpret(Point3f, vec($posObs)), hostTriIds)
-function update_plot!(posObs, s, p)
-    posObs[] =s.hostPos
-end
-
-lights = [
-    DirectionalLight(RGBf(0, 0, 0.7), Vec3f(-4, -4, 0)),
-    DirectionalLight(RGBf(0.7, 0.2, 0), Vec3f(-4, 4, -1)),
-    DirectionalLight(RGBf(0.7, 0.7, 0.7), Vec3f(4, -4, -1)),
-    AmbientLight(RGBf(0.3, 0.2, 0.2))
-]
-
-set_theme!(theme_dark())
-begin
+    set_theme!(theme_dark())
     fig = Figure(size = (800, 800)) 
-    ax = LScene(fig, scenekw = (;lights, camera = cam3d!,), show_axis = false)
+    ax = LScene(fig[1,1], scenekw = (;lights, camera = cam3d!,), show_axis = false)
 
     mesh!(msh, transparency = true, color = (:orange, 0.4), backlight = 1)
-    #surface!([-8,-8,8,8],[-8,8,-8,8],[0,0,0,0], color = :white, colormap = [:darkred])
-    #meshscatter!(@lift(vec($posObs)), markersize = 0.01, color = (:darkred, 0.5))
-    #wireframe!(msh)
     meshscatter!([Point3f(params.sphereCenter)], markersize = params.sphereRadius, color = :red)
     cam = cameracontrols(ax.scene)
     cam.lookat[] = sphereCenter
     rotate_cam!(ax.scene, cam, (0.0, -pi/5, 0.0))
     update_cam!(ax.scene, cam)
 
-    fig[1,1] = ax
-    fig 
+    display(fig) 
+
+    return fig, stateObs
 end
 
-@time simulate!(state, params, CUDABackend(), 64)
-# 0.034735 seconds (10.44 k allocations: 409.734 KiB)
-
-reset!(state)
-update_plot!(posObs, state, params)
-for i in 1:200 
-    simulate!(state, params)
-    update_plot!(posObs, state, params)
-    sleep(0.001)
+function update_plot!(stateObs, state, params)
+    stateObs[] = state
 end
+
+
+
+# reset!(state)
+# update_plot!(posObs, state, params)
+# for i in 1:200 
+#     simulate!(state, params)
+#     update_plot!(posObs, state, params)
+#     sleep(0.001)
+# end
+
+
+
+# init 
+state, params = init_state()
+fig, stateObs = init_plot(state, params)
 
 
 begin 
     reset!(state)
-    update_plot!(posObs, state, params)
+    update_plot!(stateObs, state, params)
+
     Makie.record(fig, "example.mp4", 1:200; update = false) do i 
         simulate!(state, params)
-        update_plot!(posObs, state, params)
+        update_plot!(stateObs, state, params)
 
         cam = cameracontrols(ax.scene)
-        if i == 1
+        if i == 1  # I don't know how to change initial camera with LScene...
             translate_cam!(ax.scene, cam, Vec3f(0.0, 0.0, -1.5))
             zoom!(ax.scene, cam, 0.75)
             update_cam!(ax.scene, cam)
@@ -403,15 +393,6 @@ begin
 end
 
 
-using GLMakie
-begin
-    fig = Figure() 
-    ax = LScene(fig[1,1])
-    surface!([-8,-8,8,8],[-8,8,-8,8],[0,0,0,0])
-    meshscatter!([1],[1],[1], markersize = 1)
-    zoom!(ax.scene, cameracontrols(ax.scene), 0.1)
-    #zoom!(ax.scene, cameracontrols(ax.scene), 2.0)
-    update_cam!(ax.scene, cameracontrols(ax.scene))
-    
-    fig
-end
+
+# @time simulate!(state, params, CUDABackend(), 128)
+# 0.034735 seconds (10.44 k allocations: 409.734 KiB)
